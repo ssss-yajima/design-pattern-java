0dbccd3e903effa33e7cd8a482b955f9c69b395c
y_megane_IDEA <y.megane88@gmail.com>
2019-05-14 12:37:24 +0900

    Chapter 06 Prototype

---
+++ src/chap06_Prototype/Main.java
@@ -0,0 +1,25 @@
+package chap06_Prototype;
+
+import chap06_Prototype.framework.Manager;
+import chap06_Prototype.framework.Product;
+
+public class Main {
+    public static void main(String[] args) {
+        //準備
+        Manager manager = new Manager();
+        UnderLinePen upen  = new UnderLinePen('~');
+        MessageBox mbox = new MessageBox('*');
+        MessageBox sbox = new MessageBox('/');
+        manager.register("strong message", upen);
+        manager.register("warning box", mbox);
+        manager.register("slash box", sbox);
+
+        //生成
+        Product p1 = manager.create("strong message");
+        p1.use("Hello world.");
+        Product p2 = manager.create("warning box");
+        p2.use("Hello world.");
+        Product p3 = manager.create("slash box");
+        p3.use("Hello world.");
+    }
+}
---
+++ src/chap06_Prototype/MessageBox.java
@@ -0,0 +1,32 @@
+package chap06_Prototype;
+
+import chap06_Prototype.framework.Product;
+
+public class MessageBox implements Product {
+    private char decochar;
+    public MessageBox(char decochar){
+        this.decochar = decochar;
+    }
+    public void use(String s){
+       int length = s.getBytes().length;
+       for ( int i = 0; i<length +4; i++){
+           System.out.print(decochar);
+       }
+        System.out.println("");
+        System.out.println(decochar + " " + s + " " + decochar);
+        for ( int i = 0; i<length +4; i++){
+            System.out.print(decochar);
+        }
+        System.out.println("");
+    }
+    public Product createClone(){
+        Product p = null;
+        try{
+            // clone()はJavaで規定されている。Cloneableを実装していないとCloneNotSupportedException
+            p = (Product)clone();
+        }catch(CloneNotSupportedException e){
+            e.printStackTrace();
+        }
+        return p;
+    }
+}
