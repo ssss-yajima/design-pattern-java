9d5e849b663c148a6fedbbab1ba11812e3727cca
y_megane_IDEA <y.megane88@gmail.com>
2019-05-25 00:49:34 +0900

    Chapter 15 Facade

---
+++ src/chap15_Facade/Database.java
@@ -0,0 +1,23 @@
+package chap15_Facade;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Database {
+    // newさせないためprivateコンストラクタ
+    private Database(){
+
+    }
+
+    public static Properties getProperties(String dbname) {
+        String filename = dbname + ".txt";
+        Properties prop = new Properties();
+        try {
+            prop.load(new FileInputStream(filename));
+        } catch (IOException e) {
+            System.out.println("Warning: " + filename + "is not found");
+        }
+        return prop;
+    }
+}
---
+++ src/chap15_Facade/Main.java
@@ -0,0 +1,7 @@
+package chap15_Facade;
+
+public class Main {
+    public static void main(String[] args) {
+        PageMaker.makeWelcomePage("hoge@hoge.co.jp", "welcome.html");
+    }
+}
